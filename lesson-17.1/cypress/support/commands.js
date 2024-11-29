798955283a1905efbc00718da56d0c8b16396505
Veronika_Seleznyova <vsel0438@gmail.com>
2024-11-29 15:38:02 +0200

    Fix.

--- lesson-17.1/cypress/support/commands.js
+++ lesson-17.1/cypress/support/commands.js
@@ -10,24 +10,3 @@ Cypress.Commands.add('login', (email, password) => {
 
   cy.log('Login successful!');
 });
-
-Cypress.Commands.add('createExpense', (carId, mileage, liters, totalCost) => {
-  return cy
-    .request({
-      method: 'POST',
-      url: 'https://qauto.forstudy.space/api/expenses',
-      headers: { Authorization: `Bearer ${Cypress.env('userToken')}` },
-      body: {
-        car_id: carId,
-        mileage: mileage,
-        liters: liters,
-        total_cost: totalCost,
-      },
-    })
-    .then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.expense).to.exist;
-
-      return response.body.expense;
-    });
-});
