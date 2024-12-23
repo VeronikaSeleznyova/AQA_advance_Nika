3b4606c8d5da33c2ebde2eee3b7a2d8a234df693
Veronika_Seleznyova <vsel0438@gmail.com>
2024-12-23 16:58:31 +0200

    Added small fix to config file.

--- lesson_23/playwright.config.js
+++ lesson_23/playwright.config.js
@@ -28,7 +28,6 @@ module.exports = defineConfig({
     baseURL: process.env.BASE_URL,
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
-    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
